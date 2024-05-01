f7754f046fea0f92c94bb05f57fa4b13f8cffff7
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-05-01 08:45:47 +0300

    commit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+  const timeOut = setTimeout(function () {
+      onAnswered(false)
+      setTimeRemaining(timeRemaining -1)
+    }, 1000)
+    
+    return function cleanup() {
+      clearTimeout(timeOut);
+    }
+  })
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
